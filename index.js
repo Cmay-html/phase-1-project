91c69eef686e6abdfa597a1275f8279436231105
cynthia <cynthialairumbe@gmail.com>
2025-06-24 17:52:58 +0300

    add files

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 let parks = [];
 
 //Getting elements
-const parksContainer = getElementById("park-container");
-const countySelect= getElementById("county-container");
-const activitySelect= getElementById("activity-container");
-const seachInput= getElementById("search-input");
+document.addEventListener('DOMContentLoaded', () => {
+const parksContainer = document.getElementById("park-container");
+const countySelect= document.getElementById("county-container");
+const activitySelect= document.getElementById("activity-container");
+const seachInput= document.getElementById("search-input");
+const darkModeToggle = document.getElementById('dark-mode-toggle');
 
 //Fetching Parks from json-server
 fetch('http://localhost:3000/parks')
@@ -15,7 +17,15 @@ fetch('http://localhost:3000/parks')
     })
     .catch((error) => console.error('Error fetching parks:', error));
  
+ //Rendering parks to DOM 
+ function renderParks(parks) {
+    parksContainer.innerHTML = '';
+    if (parks.length === 0)  {
+        parksContainer.innerHTML = `<p>No such park.</p>`;
+        return;
+    }
     
+
 //Event Listeners
 searchInput.addEventListener('input', () => {
   filterParks();
@@ -27,3 +37,4 @@ activitySelect.addEventListener('change', () => {
   filterParks();
 });
 darkModeToggle.addEventListener('click', toggleDarkMode);
+})
